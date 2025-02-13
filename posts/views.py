fd49107fcebff1bd2090f7ad96465b0b3baac907
xcheche <chekwubechukwu23@gmail.com>
2025-02-13 07:47:19 +0100

    changed ui

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from datetime import datetime
 
 from .models import Post
 
 # Create your views here.
 def home(request):
+    time = datetime.now()
     posts  = Post.objects.order_by('-pub_date')
-    return render(request, 'posts/home.html', {'posts':posts})
+    context = {
+        'posts': posts,
+        'time': time
+    }
+    return render(request, 'posts/home.html', context)
 
 def post_details(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
