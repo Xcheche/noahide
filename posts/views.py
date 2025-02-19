ae42d94cf2907b4ab0738b9620e92e6c750e111e
xcheche <chekwubechukwu23@gmail.com>
2025-02-19 17:34:39 +0100

    restricted details page

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 
@@ -12,7 +13,7 @@ def home(request):
         'time': time
     }
     return render(request, 'posts/home.html', context)
-
+@login_required
 def post_details(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'posts/posts_detail.html', {'post':post})
